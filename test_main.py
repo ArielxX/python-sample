675b1ca36c09c0607eb2d3edd811b7e21d81857e
Ariel Cruz Cruz <acruzx94@gmail.com>
2022-04-29 14:33:59 +0200

    fix test file

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ import unittest
 
 class MyTest(unittest.TestCase):
     def test(self):
-        f = get_message("hhtp://localhost")
-        self.assertEqual(f.Url == "hhtp://localhost")
+        f = get_message("http://localhost")
+        self.assertEqual(f["Url"], "http://localhost")
